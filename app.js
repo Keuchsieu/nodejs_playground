430c81d9745fac92326e186a4e40a640fc407557
yuqingqiu <yuqingqiu93@gmail.com>
2018-10-19 20:32:29 -0700

    hosting on firebase

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.listen(8000, ()=>{
+var server = app.listen(8000, ()=>{
     console.log("on port " + PORT);
 });
